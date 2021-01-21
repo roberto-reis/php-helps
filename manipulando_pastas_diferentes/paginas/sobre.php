472e6a1aaeaff920c6aa7770250e9bf0571f851f
José Roberto <tekinforroberto@gmail.com>
2021-01-20 21:25:50 -0300

    Manipulando pastas diferentes

---
+++ manipulando_pastas_diferentes/paginas/sobre.php
@@ -0,0 +1,5 @@
+<?php 
+    include_once "../template/estrutura/header.php";
+?>
+
+Sobre
---
+++ manipulando_pastas_diferentes/template/estrutura/header.php
@@ -0,0 +1,7 @@
+<?php
+    include_once "../config.php";
+    echo "Versão: ".$versao;
+?>
+
+<h1>Cabeçalo</h1>
+<hr>
